9bba7e0ec55da578b467851e73a41a55dc9d152d
PanConQuesitoo <86250209+PanConQuesitoo@users.noreply.github.com>
2021-06-21 12:03:16 -0500

    Add files via upload

---
+++ areatriangulo.py
@@ -0,0 +1,5 @@
+print("Programa que calcula el area de un triangulo")
+altura=float(input("Digite el valor de la altura en cm: "))
+base=float(input("Digite el valor de la base en cm: "))
+area=(altura*base)/2
+print("El valor del area del triangulo es: ",area,"cm^2")
